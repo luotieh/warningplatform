47b9e94a05a2465f25a951cafd040018e29a14dc
yt-lsa <lrj1@yt-security.com>
2026-05-21 14:57:26 +0800

    功能更新

--- vulnscan-backend/incident/stats/wire_provider.go
+++ vulnscan-backend/incident/stats/wire_provider.go
@@ -1,14 +1,13 @@
 package stats
 
 import (
-	statsContract "vulnscan-backend/incident/stats/stats-contract"
 	"vulnscan-backend/sitemonitor"
 
 	"code.yt-security.com/public/core/v2/db"
 )
 
 // ProvideServiceStats 注入站点监测对象存储，用于导出报告时嵌入截图等证据。
-func ProvideServiceStats(database *db.DB, monitor *sitemonitor.Monitor) statsContract.ServiceStats {
+func ProvideServiceStats(database *db.DB, monitor *sitemonitor.Monitor) *serviceStats {
 	var store reportObjectStore
 	if monitor != nil {
 		if ns := monitor.GetNatsService(); ns != nil {
